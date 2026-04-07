da4cc0c41be36c30ca96aa53e65ce3b31fdf2f9f
alissonnff <alisson_fmf@hotmail.com>
2026-04-07 19:56:22 -0300

    Criado a controller [Aula2]

---
+++ src/main/java/dev/franca/CadastroDeNinjas/Controller.java
@@ -0,0 +1,17 @@
+package dev.franca.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Controller
+@RequestMapping // Anotation para as rotas
+
+public class Controller {
+
+    @GetMapping("/boasvindas") // Anotation que "pega" informações do meu metodo;
+    public String boasVindas(){
+
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+}
